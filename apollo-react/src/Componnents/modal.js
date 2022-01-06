639436a7108f48f5861216a8801e49d445c95082
Gregory norvene <gnorvene@gmail.com>
2022-01-06 01:30:07 +0100

    fix:  modal  update  good item  on list

---
+++ apollo-react/src/Componnents/modal.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+
+import { DELETTE_COMPAGNY, UPDATE_COMPAGNY } from "../Graphql/Mutation";
+import { LOAD_COMPAGNIES } from "../Graphql/Query";
+import { useMutation } from "@apollo/client";
+export default function UsingHookModal(props) {
+  const [modalShow, setShow] = useState(false);
+
+  let [name, setName] = useState("");
+  let compagny = props.item;
+  const [deletteCompagny] = useMutation(DELETTE_COMPAGNY, {
+    refetchQueries: [LOAD_COMPAGNIES],
+  });
+  const [updateCompagny] = useMutation(UPDATE_COMPAGNY, {
+    refetchQueries: [LOAD_COMPAGNIES],
+  });
+  const handleUpdate = (e) => {
+    updateCompagny({
+      variables: { id: e, name: name },
+    });
+    setShow(false);
+  };
+  const handleDelette = (e) => {
+    let idCompagny = e;
+    deletteCompagny({ variables: { id: idCompagny } });
+    setShow(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={() => setShow(true)}>Edit</Button>{" "}
+      <Modal
+        size="sm"
+        show={modalShow}
+        onHide={() => setShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">Compagny</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          <input
+            type="text"
+            value={name}
+            placeholder={compagny.name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => handleDelette(compagny.id)}
+          >
+            DELETE
+          </Button>
+          <Button
+            variant="primary"
+            onClick={(e) => handleUpdate(compagny.id, name)}
+          >
+            SAVE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
--- apollo-react/src/pages/CompagnyList.js
+++ apollo-react/src/pages/CompagnyList.js
@@ -1,42 +1,10 @@
-import React, { useState } from "react";
-import { useMutation, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import ListGroup from "react-bootstrap/ListGroup";
 import { LOAD_COMPAGNIES } from "../Graphql/Query";
-import { Button, Modal } from "react-bootstrap";
-import AddForm from "../Componnents/AddForm";
-import { DELETTE_COMPAGNY, UPDATE_COMPAGNY } from "../Graphql/Mutation";
-export default function CompagnyList() {
+import UsingHookModal from "../Componnents/modal";
+
+export default function CompagnyList(props) {
   const { error, loading, data } = useQuery(LOAD_COMPAGNIES);
-
-  const [deletteCompagny] = useMutation(DELETTE_COMPAGNY, {
-    refetchQueries: [LOAD_COMPAGNIES],
-  });
-
-  const [updateCompagny] = useMutation(UPDATE_COMPAGNY, {
-    refetchQueries: [LOAD_COMPAGNIES],
-  });
-
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-
-  const handleShow = () => setShow(true);
-
-  const handleDelette = (e) => {
-    let idCompagny = e;
-    deletteCompagny({ variables: { id: idCompagny } });
-    setShow(false);
-  };
-
-  const handleUpdate = (e) => {
-    let idCompagny = e.id;
-    let nameCompagny = e.name;
-    console.log(idCompagny + " " + nameCompagny);
-    updateCompagny({
-      variables: { id: idCompagny, name: "123" },
-    });
-    setShow(false);
-  };
 
   if (loading) return <div>spiner....</div>;
   if (error) return <div>something went wrong </div>;
@@ -45,38 +13,11 @@ export default function CompagnyList() {
     <div style={{ display: "block", width: 700, padding: 30 }}>
       {data.getCompagnies.map((compagny) => {
         return (
-          <ListGroup key={compagny.id}>
-            <ul class="list-group cProductsList">
-              <li class="list-group-item d-flex">
-                <p class="p-0 m-0 flex-grow-1">{compagny.name}</p>
-
-                <Button class="btn-success" onClick={handleShow}>
-                  Edit
-                </Button>
-                <Modal show={show} onHide={handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Update Compagny</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <AddForm name={compagny.name} />
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <form>
-                      <Button
-                        variant="secondary"
-                        onClick={(e) => handleDelette(compagny.id)}
-                      >
-                        Delete
-                      </Button>
-                      <Button
-                        variant="primary"
-                        onClick={(e) => handleUpdate(compagny)}
-                      >
-                        Save Changes
-                      </Button>
-                    </form>
-                  </Modal.Footer>
-                </Modal>
+          <ListGroup key={compagny.name}>
+            <ul className="list-group cProductsList">
+              <li className="list-group-item d-flex" key={compagny.id}>
+                <p className="p-0 m-0 flex-grow-1">{compagny.name} </p>
+                <UsingHookModal item={compagny} />
               </li>
             </ul>
           </ListGroup>
